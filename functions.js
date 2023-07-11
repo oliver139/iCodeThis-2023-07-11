aacf85f4837f5b157ac329507090c0fc63baf1a6
Oliver Mak <oliver139.working@gmail.com>
2023-07-11 21:17:05 +0800

    tailwind msg

--- functions.js
+++ functions.js
@@ -1,8 +1,20 @@
 window.onload = async e => {
-  const container = document.querySelector('.container');
-  container.addEventListener("click", e => {
-    const box = document.createElement('div');
-    box.classList.add('box');
-    container.appendChild(box);
+  const msgbar = document.getElementById("msgbar");
+  const closeMsg = document.getElementById("closeMsg");
+
+  const hideMsg = () => {
+    msgbar.addEventListener("transitionend", e => {
+      msgbar.remove();
+    }); 
+    msgbar.classList.add("hide");
+  }
+
+  const timer = setTimeout(() => {
+    hideMsg();
+  }, 5000);
+
+  closeMsg.addEventListener("click", e => {
+    clearTimeout(timer);
+    hideMsg();
   });
 }
